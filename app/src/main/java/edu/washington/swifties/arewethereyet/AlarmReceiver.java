62bbb7486699c09810b89b76826dc67420b087df
Yulong Tan <yulongtan67@gmail.com>
2017-02-18 19:21:27 -0800

    fix fields not filled bug

--- app/src/main/java/edu/washington/swifties/arewethereyet/AlarmReceiver.java
+++ app/src/main/java/edu/washington/swifties/arewethereyet/AlarmReceiver.java
@@ -3,6 +3,7 @@ package edu.washington.swifties.arewethereyet;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 /**
@@ -13,6 +14,7 @@ public class AlarmReceiver extends BroadcastReceiver {
 
   @Override
   public void onReceive(Context context, Intent intent) {
+    Log.d("onReceive", "class is called");
     String phoneNumber = intent.getStringExtra(MainActivity.PHONE_NUMBER);
     Toast.makeText(context, phoneNumber + ": Are we there yet?", Toast.LENGTH_SHORT).show();
   }
--- app/src/main/java/edu/washington/swifties/arewethereyet/MainActivity.java
+++ app/src/main/java/edu/washington/swifties/arewethereyet/MainActivity.java
@@ -7,9 +7,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -18,7 +20,6 @@ public class MainActivity extends Activity {
   private boolean fieldsAreFilled = true;
 
   public static final String PHONE_NUMBER = "phone_number";
-  public static final int INTERVAL = 10000;
 
   private PendingIntent pi;
 
@@ -39,17 +40,33 @@ public class MainActivity extends Activity {
     startStopButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        if (messageEditText.getText().length() == 0 ||
-            phoneEditText.getText().length() == 0 ||
-            intervalEditText.getText().length() == 0) {
+        // Checks if all the fields are filled out
+        if (messageEditText.getText().toString().length() == 0 ||
+            phoneEditText.getText().toString().length() == 0 ||
+            intervalEditText.getText().toString().length() == 0) {
           fieldsAreFilled = false;
+        } else {
+          fieldsAreFilled = true;
         }
 
-        alarmIntent.putExtra(PHONE_NUMBER, phoneEditText.getText().toString());
+        // Tells user that fields are not filled
+        if (!fieldsAreFilled) {
+          Toast.makeText(MainActivity.this, "Cannot start: Not all fields are filled",
+              Toast.LENGTH_SHORT).show();
+        }
+
+        Log.d("start", "Message length: " + messageEditText.getText().toString().length());
+        Log.d("start", "Phone length: " + phoneEditText.getText().toString().length());
+        Log.d("start", "interval length: " + messageEditText.getText().toString().length());
 
         // Do something if all the fields are filled up
         if (fieldsAreFilled) {
-          // Starts the service
+          // Passes the user given phone number
+          alarmIntent.putExtra(PHONE_NUMBER, phoneEditText.getText().toString());
+
+          // Starts the alarm service
+          // When start is clicked, the text shows stop and vice versa
+          // Start text is green and cancel text is red
           if (startStopButton.getText().toString().equals("START")) {
             startStopButton.setText("CANCEL");
             startStopButton.setTextColor(Color.RED);
@@ -62,14 +79,16 @@ public class MainActivity extends Activity {
         }
       }
     });
-
   }
 
+  // Begins the alarm manager with the user given interval
   private void startAlarm() {
     AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-    manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), INTERVAL, pi);
+    manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),
+        Integer.parseInt(intervalEditText.getText().toString()), pi);
   }
 
+  // Cancels the alarm (so it stops running in the background as well)
   private void stopAlarm() {
     AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
     manager.cancel(pi);
